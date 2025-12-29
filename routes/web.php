34e3ec92a7467278cc6a6d11678ce0a10984b0a9
Pisoth567 <pisoth887.com>
2025-12-29 10:38:59 +0700

    connected to many routes so i forgot something i will update it later

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,14 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [UserController::class, 'home'])->name('index');
+Route::get('/product_detail/{id}', [UserController::class, 'productDetail'])->name('product_detail');
+Route::get('/viewallproduct', [UserController::class, 'allProducts'])->name('viewallproduct');
 
 Route::get('/dashboard', [UserController::class , "index"])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/addtocard/{id}', [UserController::class , "addToCard"])->middleware(['auth', 'verified'])->name('add_to_card');
+Route::get('/removecartproduct/{id}', [UserController::class , "removeCartProduct"])->middleware(['auth', 'verified'])->name('removecartproduct');
+
+Route::get('/cartproducts', [UserController::class , "cartProducts"])->middleware(['auth', 'verified'])->name('cartproducts');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
